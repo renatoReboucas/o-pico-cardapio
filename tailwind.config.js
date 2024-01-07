bb88271381ad2a1343c5b70b2a6362e3d2510847
renatoReboucas <renato.silva@triforsys.com.br>
2024-01-07 01:45:39 -0300

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,8 @@ module.exports = {
     extend: {
       fontFamily: {
         roboto: 'var(--font-roboto)',
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      }
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'),],
 }
