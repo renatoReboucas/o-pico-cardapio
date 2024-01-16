086b423a83b26a2f298cd79a8eb9328256d7a713
renatoReboucas <renato.silva@triforsys.com.br>
2024-01-15 21:03:59 -0300

    wip

--- src/app/api/menu/route.js
+++ src/app/api/menu/route.js
@@ -1,185 +1,109 @@
-import { NextRequest, NextResponse } from "next/server";
+import "dotenv/config"
+import { NextResponse } from "next/server";
+import {Client} from '@notionhq/client'
+// https://www.notion.so/renatoreboucas/824be7a1be4d45eda301240a9defdb3f?v=dcd902f5af384372bb3ce8939d793560&pvs=4
+const DATABASE_ID = '824be7a1be4d45eda301240a9defdb3f'
+const NOTION_API_KEY = process.env.API_KEY
+const notion = new Client({ auth: NOTION_API_KEY })
 
-export async function GET(request) {
-  const data = [
-    {
-      section: "Clássico burguers",
-      menu: [
-        {
-          name: "Capital inicial",
-          description:
-            "Pão tipo brioche, carne 150g, queijo e maionese da casa.",
-          price: 20,
-        },
-        {
-          name: "maneva",
-          description:
-            "Pão tipo brioche, linguiça toscana 150g, queijo, alface, tomate, cebola roxa e maionese da casa.",
-          price: 24,
-        },
-        {
-          name: "Pitty",
-          description:
-            "Pão tipo brioche, carne 150g, queijo, alface, tomate, cebola roxa e maionese da casa.",
-          price: 24,
-        },
-        {
-          name: "Cpm22",
-          description:
-            "Pão tipo brioche, carne 150g, bacon, queijo e maionese da casa.",
-          price: 25,
-        },
-      ],
-    },
-    {
-      section: "especial burguers",
-      menu: [
-        {
-          name: "Raimundos",
-          description:
-            "Pão tipo brioche, carne 150g, cheddar cremoso, doritos e maionese da casa.",
-          price: 27,
-        },
-        {
-          name: "revelação",
-          description:
-            "Pão tipo brioche, 2x carne 150g, queijo, batata pringles e maionese da casa.",
-          price: 31,
-        },
-        {
-          name: "mamonas",
-          description:
-            "Pão tipo brioche, carne 150g, carne cozida e desfiada, queijo e maionese da casa.",
-          price: 27,
-        },
-        {
-          name: "tim maia",
-          description:
-            "Pão tipo brioche, 3x carne 150g, bacon, queijo e maionese da casa.",
-          price: 37,
-        },
-      ],
-    },
-    {
-      section: "Baguetes de parmesão",
-      menu: [
-        {
-          name: "hot louco",
-          description:
-            "Duas salsichas, tomate, cebola roxa, catupiry, mussarela, bacon, batata palha e maionese da casa.",
-          price: 21,
-        },
-        {
-          name: "frango louco",
-          description:
-            "Frango cozido e desfiado, catupiry, batata palha e maionese da casa.",
-          price: 25,
-        },
-        {
-          name: "carne louca",
-          description:
-            "Carne cozida e desfiada, mussarela, batata palha e maionese da casa.",
-          price: 28,
-        },
-        {
-          name: "porco louco",
-          description:
-            "Pernil cozido e desfiado, mussarela, batata palha e maionese da casa.",
-          price: 26,
-        },
-        {
-          name: "Nordestão",
-          description:
-            "Carne seca cozida e refogada na manteiga, catupiry e maionese da casa.",
-          price: 31,
-        },
-      ],
-    },
-    {
-      section: "porções",
-      menu: [
-        {
-          name: "Batata simples",
-          description: "",
-          price: 18,
-        },
-        {
-          name: "Batata cheddar e bacon",
-          description: "",
-          price: 24,
-        },
-        {
-          name: "batata com carne cozida e catupiry",
-          description: "",
-          price: 27,
-        },
-        {
-          name: "onion rings",
-          description: "",
-          price: 25,
-        },
-        {
-          name: "mini salgados especiais",
-          description: "",
-          price: 22,
-        },
-      ],
-    },
-    {
-      section: "refrigerantes",
-      menu: [
-        {
-          name: "guaraná antarctica 310ml",
-          description: "",
-          price: 6,
-        },
-        {
-          name: "coca cola 310ml",
-          description: "",
-          price: 6,
-        },
-        {
-          name: "fanta laranja",
-          description: "",
-          price: 6,
-        },
-        {
-          name: "fanta uva 350ml",
-          description: "",
-          price: 6,
-        },
-      ],
-    },
-    {
-      section: "Adicionais",
-      menu: [
-        {
-          name: "catupiry",
-          description: "",
-          price: 5,
-        },
-        {
-          name: "ovo",
-          description: "",
-          price: 3,
-        },
-        {
-          name: "hamburguer",
-          description: "",
-          price: 10,
-        },
-        {
-          name: "salada",
-          description: "",
-          price: 3,
-        },
-        {
-          name: "bacon",
-          description: "",
-          price: 4,
-        },
-      ],
-    },
-  ];
-  return NextResponse.json(data);
+export async function GET() {
+  const response = await notion.databases.query({
+    database_id: DATABASE_ID,
+
+  })
+  // return response
+  // console.log(response.results[0].properties);
+  const arrMenu = []
+  const specialBurguerFilter = response.results.filter( item => item?.properties?.section?.multi_select[0]?.name === 'Especial burguers')
+  const specialBurguer = specialBurguerFilter.map(item =>{
+    return {
+      description: item?.properties?.description?.rich_text[0]?.plain_text,
+        photo:  item?.properties?.photo?.rich_text[0] ? item?.properties?.photo?.rich_text[0]?.plain_text : '',
+        price: item?.properties?.price?.number,
+        name: item?.properties?.name?.title[0]?.plain_text,
+    }
+  })
+  arrMenu.push({
+    section: 'Especial burguers',
+    menu: specialBurguer
+  })
+  const classicoBurguersFilter = response.results.filter( item => item?.properties?.section?.multi_select[0]?.name === 'Clássico burguers')
+  const classicoBurguers = classicoBurguersFilter.map(item =>{
+    return {
+      description: item?.properties?.description?.rich_text[0]?.plain_text,
+        photo:  item?.properties?.photo?.rich_text[0] ? item?.properties?.photo?.rich_text[0]?.plain_text : '',
+        price: item?.properties?.price?.number,
+        name: item?.properties?.name?.title[0]?.plain_text,
+    }
+  })
+  arrMenu.push({
+    section: 'Clássico burguers',
+    menu: classicoBurguers
+  })
+  const baguetesFilter = response.results.filter( item => item?.properties?.section?.multi_select[0]?.name === 'Baguetes de parmesão')
+  const baguetesBurguers = baguetesFilter.map(item =>{
+    return {
+      description: item?.properties?.description?.rich_text[0]?.plain_text,
+        photo:  item?.properties?.photo?.rich_text[0] ? item?.properties?.photo?.rich_text[0]?.plain_text : '',
+        price: item?.properties?.price?.number,
+        name: item?.properties?.name?.title[0]?.plain_text,
+    }
+  })
+  arrMenu.push({
+    section: 'Baguetes de parmesão',
+    menu: baguetesBurguers
+  })
+  const porcoesFilter = response.results.filter( item => item?.properties?.section?.multi_select[0]?.name === 'porções')
+  const porcoesBurguers = porcoesFilter.map(item =>{
+    return {
+      description: item?.properties?.description?.rich_text[0] ? item?.properties?.description?.rich_text[0]?.plain_text : '',
+        photo:  item?.properties?.photo?.rich_text[0] ? item?.properties?.photo?.rich_text[0]?.plain_text : '',
+        price: item?.properties?.price?.number,
+        name: item?.properties?.name?.title[0]?.plain_text,
+    }
+  })
+  arrMenu.push({
+    section: 'porções',
+    menu: porcoesBurguers
+  })
+  const refrigerantesFilter = response.results.filter( item => item?.properties?.section?.multi_select[0]?.name === 'refrigerantes')
+  const refrigerantesBurguers = refrigerantesFilter.map(item =>{
+    return {
+      description: item?.properties?.description?.rich_text[0] ? item?.properties?.description?.rich_text[0]?.plain_text : '',
+        photo:  item?.properties?.photo?.rich_text[0] ? item?.properties?.photo?.rich_text[0]?.plain_text : '',
+        price: item?.properties?.price?.number,
+        name: item?.properties?.name?.title[0]?.plain_text,
+    }
+  })
+  arrMenu.push({
+    section: 'refrigerantes',
+    menu: refrigerantesBurguers
+  })
+  const adicionaisFilter = response.results.filter( item => item?.properties?.section?.multi_select[0]?.name === 'Adicionais')
+  const adicionaisBurguers = adicionaisFilter.map(item =>{
+    return {
+      description: item?.properties?.description?.rich_text[0] ? item?.properties?.description?.rich_text[0]?.plain_text : '',
+        photo:  item?.properties?.photo?.rich_text[0] ? item?.properties?.photo?.rich_text[0]?.plain_text : '',
+        price: item?.properties?.price?.number,
+        name: item?.properties?.name?.title[0]?.plain_text,
+    }
+  })
+  arrMenu.push({
+    section: 'adicionais',
+    menu: adicionaisBurguers
+  })
+  return NextResponse.json(arrMenu);
+  const formatResponse = response.results.map( item  =>  {
+    const data = {
+      section: item?.properties?.section?.multi_select[0]?.name,
+      menu: {
+        description: item?.properties?.description?.rich_text[0]?.plain_text,
+        photo:  item?.properties?.photo?.rich_text[0] ? item?.properties?.photo?.rich_text[0]?.plain_text : '',
+        price: item?.properties?.price?.number,
+        name: item?.properties?.name?.title[0]?.plain_text,
+      }
+    }
+    return data
+  })
+  return NextResponse.json(formatResponse);
 }
