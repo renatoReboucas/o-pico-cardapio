7ffb51426a09af2611085bb056d9890231be82b6
renatoReboucas <renato.silva@triforsys.com.br>
2024-01-26 11:17:56 -0300

    wip

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   images:{
     remotePatterns:[{
-      protocol:'http',
-      hostname: 'localhost',
-      port: '3000'
+      protocol:'https',
+      hostname: 'o-pico-cardapio.vercel.app/',
+      port: ''
     }]
   }
 }
